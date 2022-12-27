8f0da762320454cfb31794609d0eeb77a695e72a
atsed27 <119629453+atsed27@users.noreply.github.com>
2022-12-27 01:39:32 -0500

    First commit

---
+++ client.js
@@ -0,0 +1,16 @@
+import net from "net"
+
+const port = 3000;
+const host = "127.0.0.1";
+
+try {
+    const client = new net.Socket()
+    client.connect({ port: port, host: host }, () => {
+        console.log('Client is alive and establishing socket connection. \n');
+    })
+    client.on("data", (data) => {
+        console.log(data.toString());
+    });
+} catch (error) {
+    console.log('Error establishing connection')
+}
---
+++ server.js
@@ -0,0 +1,25 @@
+import net from 'net';
+
+const host = "127.0.0.1";
+
+const port = 3000;
+const SIM_LENGTH = 10;
+
+try {
+    const server = net.createServer((socket) => {
+
+        for (let count = 0; count <= SIM_LENGTH; count++) {
+            socket.write(`connect_socket ${count} \n`)
+            console.log(`Server has written ${count} to socket \n`);
+
+        }
+        socket.end();
+
+
+    });
+    server.listen(port, () => {
+        console.log("Server is alive and wating for socket connection from client Information.\n");
+    });
+} catch (error) {
+    console.error('Socket error')
+}
